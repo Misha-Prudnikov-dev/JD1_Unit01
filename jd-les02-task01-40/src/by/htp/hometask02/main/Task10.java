7dcf5b8e0fe1a6504ecc9c34f1a52e3ab9aee7e6
Misha-Prudnikov-dev <54108720+Misha-Prudnikov-dev@users.noreply.github.com>
2019-08-15 12:56:16 +0300

    Update Task10.java

--- jd-les02-task01-40/src/by/htp/hometask02/main/Task10.java
+++ jd-les02-task01-40/src/by/htp/hometask02/main/Task10.java
@@ -9,13 +9,16 @@ public class Task10 {
 
 		double r1 = 3;
 		double s1;
+		
 		s1 = Math.pow(r1, 2) * Math.PI;
 
 		double r2 = 3;
 		double s2;
+		
 		s2 = Math.pow(r2, 2) * Math.PI;
 
 		double xMin;
+		
 		xMin = Math.min(s1, s2);
 
 		System.out.println("Площадь круга "+xMin);
